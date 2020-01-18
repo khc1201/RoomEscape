24891cd89044cc6b3ec891358b109bfad20c3bf5
khc1201 <41064972+khc1201@users.noreply.github.com>
2020-01-18 22:17:00 +0900

    이동 버튼의 Image 의 보임 / 숨김 추가
    + 목적
     - 이동 가능한 곳을 확실하게 인지시켜주기 위해서 이동 버튼의 Image 버튼의 보이기 / 숨김 조건을 추가했다.
    
    + 연관 work : #2
    
    + 상세
     - CameraMoveButton 에 m_image 를 추가
     - m_image 는 SetButton 에서 true 조건에 따라 enable 값이 변경된다

--- Dream/Assets/02.Scripts/03.Buttons/CameraMoveButton.cs
+++ Dream/Assets/02.Scripts/03.Buttons/CameraMoveButton.cs
@@ -9,6 +9,7 @@ public class CameraMoveButton : MonoBehaviour
     private Button m_button;
     private GameObject m_text;
     private CameraManager m_cameraManager;
+    private Image m_image;
 
     [Header("+ 클릭시 CameraManager 로 보낼 정보")]
     public MoveDir m_moveDir;
@@ -23,11 +24,13 @@ public class CameraMoveButton : MonoBehaviour
         m_button = transform.GetComponent<Button>();
         m_text = transform.GetComponentInChildren<Text>().gameObject;
         m_cameraManager = FindObjectOfType<CameraManager>();
+        m_image = transform.GetComponent<Image>();
     }
     public void SetButton(bool isTrue)
     {
         m_button.enabled = isTrue;
         m_text.SetActive(isTrue);
+        m_image.enabled = isTrue;
     }
 
     public void OnClickButton()
