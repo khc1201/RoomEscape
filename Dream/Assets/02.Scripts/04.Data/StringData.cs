3cdd6d7d91d51e5335151ae32ae7c691412c5118
khc1201 <41064972+khc1201@users.noreply.github.com>
2020-05-05 22:08:15 +0900

    스트링 줄바꿈 구현
    + 목적
     - 가독성 향상을 위해 엑셀 단위에서 스트링 줄바꿈에 대한 태그를 입력
    
    + 연관 work :  #37
    
    + 상세
     - \n 태그를 입력하면 줄바꿈 처리가 됨

--- Dream/Assets/02.Scripts/04.Data/StringData.cs
+++ Dream/Assets/02.Scripts/04.Data/StringData.cs
@@ -18,13 +18,24 @@ public class StringData
     public StringData(string index, string lang_1, string lang_2, string lang_3, string lang_4, string lang_5, string lang_6, string soundEffect, string color)
     {
         Index = index;
-        Lang_1 = lang_1;
-        Lang_2 = lang_2;
-        Lang_3 = lang_3;
-        Lang_4 = lang_4;
-        Lang_5 = lang_5;
-        Lang_6 = lang_6;
+        Lang_1 = ReplaceTag(lang_1);
+        Lang_2 = ReplaceTag(lang_2);
+        Lang_3 = ReplaceTag(lang_3);
+        Lang_4 = ReplaceTag(lang_4);
+        Lang_5 = ReplaceTag(lang_5);
+        Lang_6 = ReplaceTag(lang_6);
+
         PlaySoundEffect = soundEffect;
         FontColor = color;
+    }
+
+    private string ReplaceTag(string target)
+    {
+        //타겟 스트링에 대해 기존 Tag 를 실제 적용되는 스트링으로 변환하는 작업을 한다.
+        string tempString = target;
+        tempString.Replace("\\n", "\n");
+        //for test
+        Debug.Log(tempString);
+        return tempString;
     }
 }
